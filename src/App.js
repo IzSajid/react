90711368e0e355ac6b38682121f1cf569311a1ad
Imtiaz Sajid <96991963+IzSajid@users.noreply.github.com>
2023-10-20 19:04:45 +0600

    Upto 6
    Proper Way to change the values

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Employee from './components/employee';
+import { useState } from 'react'; 
 function App() {
+  const [role, setRole] = useState(); // [state, setState
   const showEmployee = true;
   return (
     <div className="App">
       {showEmployee ? (
         <>
-          <Employee name = "Sajid" role = "Intern"/>
-          <Employee name = "Caleb" role = "Senior Dev"/>
-          <Employee name = "Joker"/>
+          <input type='text' onChange={(e) => {
+            console.log(e.target.value)
+            setRole(e.target.value)
+          }
+          } />
+          <Employee name="Sajid" role="Intern" />
+          <Employee name="Caleb" role="Senior Dev" />
+          <Employee name="Aby" role={role} />
         </>
       ) : (
         <h3> You do not have acess</h3>
