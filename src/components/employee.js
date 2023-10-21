bfdd1c4f8d3edd41faf1f6d65b4b37b7d740b002
Imtiaz Sajid <96991963+IzSajid@users.noreply.github.com>
2023-10-21 20:48:59 +0600

    pass components done

--- src/components/employee.js
+++ src/components/employee.js
@@ -3,7 +3,7 @@ import EditEmployee from "./EditEmployee";
 function Employee(props) {
     return (
         <div className="min-w-[350px] m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
+            <img className="object-cover w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
             <div className="text-center space-y-2 sm:text-left">
                 <div className="space-y-0.5">
                     <p className="text-lg text-black font-semibold">
@@ -14,12 +14,7 @@ function Employee(props) {
                     </p>
                 </div>
 
-                <EditEmployee 
-                id = {props.id}
-                name = {props.name} 
-                role = {props.role}
-                updateEmployee = {props.updateEmployee}
-                />
+                {props.editEmployee}
 
                 
             </div>
