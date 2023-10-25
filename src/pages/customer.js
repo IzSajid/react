016bda52182c9b976e654e3392a9d39946bbe022
Imtiaz Sajid <96991963+IzSajid@users.noreply.github.com>
2023-10-26 00:07:44 +0600

    Till  42
    Done with customers styling and functionality

--- src/pages/customer.js
+++ src/pages/customer.js
@@ -1,47 +1,169 @@
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from "react";
-import NotFound from '../components/NotFound';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { baseUrl } from '../share';
 
 export default function Customer() {
     const { id } = useParams();
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
     const [customer, setCustomer] = useState();
-    const [notFound, setNotFound] = useState(false);
-    //const [error, setError] = useState(false); // added empty dependency array
+    const [tempCustomer, setTempCustomer] = useState();
+    const [notFound, setNotFound] = useState();
+    const [changed, setChanged] = useState(false);
+    const [error, setError] = useState(false);
+
     useEffect(() => {
-        const url = baseUrl + "/api/customers/" + id;
+        if (!customer) return;
+        if (!customer) return;
+
+        let equal = true;
+        if (customer.name !== tempCustomer.name) equal = false;
+        if (customer.industry !== tempCustomer.industry) equal = false;
+
+        if (equal) setChanged(false);
+    });
+
+    useEffect(() => {
+        const url = baseUrl + 'api/customers/' + id;
         fetch(url)
             .then((response) => {
                 if (response.status === 404) {
+                    //render a 404 component in this page
                     setNotFound(true);
                 }
-               return response.json()
+                if (!response.ok)
+                    throw new Error('Something went wrong!');
+
+                return response.json();
             })
             .then((data) => {
                 setCustomer(data.customer);
+                setTempCustomer(data.customer);
+                setError(undefined);
+            })
+            .catch((e) => {
+                console.log(e);
+                setError(e.message);
             });
+    }, []);
 
-    }); // added empty dependency array
+    function updateCustomer(e) {
+        e.preventDefault();
+        const url = baseUrl + 'api/customers/' + id;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(tempCustomer),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong!');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setCustomer(data.customer);
+                setChanged(false);
+                console.log(data);
+                setError(undefined);
+            })
+            .catch(
+                (e) => {
+                    console.log(e);
+                    setError(e.message);
+                }
+            );
+    }
 
     return (
-
         <>
-            {notFound ?
-            <>
-             <NotFound/> 
-             <p className='text-center text-gray-600 mt-4'>No customer with id: {id}</p>
-             </>
-             : null}
-            {customer ?
-                <div className="bg-gray-100 p-4 rounded-lg">
-                    <h1 className="text-3xl font-bold mb-2">{customer.name}</h1>
-                    <p className="text-gray-700 text-lg mb-2">ID: {customer.id}</p>
-                    <p className="text-gray-700 text-lg">Industry: {customer.industry}</p>
-                    <Link to='/customers'>Go back</Link>
-                </div>
-                : null}
+            {notFound ? <p>The customer with id {id} was not found</p> : null}
+
+            {customer ? (
+                <>
+                    <div className="items-center justify-center">
+                        <form id ='customer' onSubmit={updateCustomer} className="flex flex-col items-center justify-center">
+                            <label for='name' className="block text-gray-700 font-bold mb-2">Name</label>
+                            <input
+                                className="w-1/2 p-2 my-2 border border-gray-400 rounded-md"
+                                id='name'
+                                type="text"
+                                value={tempCustomer.name}
+                                onChange={(e) => {
+                                    setChanged(true);
+                                    setTempCustomer({
+                                        ...tempCustomer,
+                                        name: e.target.value,
+                                    });
+                                }}
+                            />
+                            <label htmlFor='industry' className="block text-gray-700 font-bold mb-2">Industry</label>
+                            <input
+                                className="w-1/2 p-2 my-2 border border-gray-400 rounded-md"
+                                id='industry'
+                                type="text"
+                                value={tempCustomer.industry}
+                                onChange={(e) => {
+                                    setChanged(true);
+                                    setTempCustomer({
+                                        ...tempCustomer,
+                                        industry: e.target.value,
+                                    });
+                                }}
+                            />
+                        </form>
+                        {changed ? (
+                            <div className="flex justify-center">
+                                <button
+                                    className="px-4 py-2 my-2 mr-2 text-white bg-gray-500 rounded-md hover:bg-gray-600"
+                                    onClick={(e) => {
+                                        setTempCustomer({ ...customer });
+                                        setChanged(false);
+                                    }}
+                                >
+                                    Cancel
+                                </button>
+                                <button form='customer' className="px-4 py-2 my-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
+                                    Save
+                                </button>
+                            </div>
+                        ) : null}
+
+                    </div>
+                    {error ? <p className="text-red-500 text-center">{error}</p> : null}
+                    <button
+                        className="px-4 py-2 my-2 text-white bg-red-500 rounded-md hover:bg-red-600"
+                        onClick={(e) => {
+                            const url = baseUrl + 'api/customers/' + id;
+                            fetch(url, {
+                                method: 'DELETE',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                            })
+                                .then((response) => {
+                                    if (!response.ok) {
+                                        throw new Error('Something went wrong');
+                                    }
+                                    navigate('/customers');
+                                })
+                                .catch((e) => {
+                                    console.log(e);
+                                });
+                        }}
+                    >
+                        Delete
+                    </button>
+                </>
+            ) : null}
+            <br />
+            <Link
+                className="text-center block mt-4 no-underline text-blue-500 hover:text-blue-700 font-bold text-sm transition duration-300 ease-in-out"
+                to="/customers"
+            >
+                <p>← Back to customers list</p>
+            </Link>
         </>
     );
-
 }
--- src/pages/customers.js
+++ src/pages/customers.js
@@ -1,27 +1,57 @@
-import { useEffect,useState } from "react";  
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { baseUrl } from '../share';
+import AddCustomer from "../components/AddCustomer";
+
+
 export default function Customers() {
-    const url = baseUrl+"api/customers";
+    const url = baseUrl + "api/customers";
     const [customers, setCustomers] = useState(); // added empty dependency array
+
+    function newCustomer(name, industry) {
+        const data = { name: name, industry: industry };
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setCustomers([...customers, data.customer]);
+            })
+            .catch(error => console.log(error));
+    }
+
     useEffect(() => {
         console.log("customers.js fetching...");
         fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            setCustomers(data.customers);
-        });
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setCustomers(data.customers);
+            });
     }, [url]); // added empty dependency array
 
     return (
-        <>
-            <h1>Customers:</h1>
-                {customers
-                    ? customers.map((customer) => {
-                    return <Link to={'/customers/' + customer.id}><p>{customer.name}</p></Link>
-                }): null}
-        </>
+        <div>
+            <h1 className="text-center">Customers:</h1>
+            {customers
+                ? customers.map((customer) => {
+                    return (
+                        <div className="text-center"
+                         key={customer.id}>
+                            <Link to={"/customers/" + customer.id}>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-2 py-2 px-4 rounded w-50 h-16">
+                                    {customer.name}
+                                </button>
+                            </Link>
+                        </div>
+                    );
+                }) : null}
+             <AddCustomer newCustomer={newCustomer} />
+        </div>
     );
-    
 }
